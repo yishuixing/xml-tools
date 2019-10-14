c8fb6ce38705b7f4e79e980d36d68d3e15ab2d62
anjunact <anjunact@qq.com>
2019-10-14 17:49:53 +0800

    xsd to java

---
+++ xml-utils/src/main/java/com/what/xml/XmlUtil.java
@@ -0,0 +1,15 @@
+package com.what.xml;
+
+public class XmlUtil {
+    public static void main(String[] args) {
+
+    }
+
+    public void toXml(Object o) {
+
+    }
+
+    public void toBean(String file) {
+
+    }
+}
